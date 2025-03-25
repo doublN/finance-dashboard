f76827afcf99931a143d373830dc40e0c7d2ba11
doublN <n.newberry@hotmail.com>
2025-03-25 16:21:14 +0000

    set up base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LeftMenu from "./components/LeftMenu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex">
+          <div className="flex-1/6">
+            <LeftMenu />
+          </div>
+          <div className="flex-5/6">{children}</div>
+        </div>
       </body>
     </html>
   );
