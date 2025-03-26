77d09b35258420d45e04f4d6c4238e5e74a6d393
doublN <n.newberry@hotmail.com>
2025-03-26 23:21:40 +0000

    create dashboard skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
 export default function Home() {
-  return <div>RIGHT</div>;
+  return (
+    <div className="h-full flex flex-col gap-4 my-10 mx-6">
+      <div className="border-2 border-gray-200 rounded-lg bg-white">
+        Tracker
+      </div>
+      <div className="grid grid-cols-2 grid-rows-2 grow gap-4">
+        <div className="row-span-2 border-2 border-gray-200 rounded-lg bg-white">
+          My Portfolio
+        </div>
+        <div className="border-2 border-gray-200 rounded-lg bg-white">
+          stats
+        </div>
+        <div className="border-2 border-gray-200 rounded-lg bg-white">
+          Fear and Greed
+        </div>
+      </div>
+      <div className="grow border-2 border-gray-200 rounded-lg bg-white">
+        Crypto Market
+      </div>
+    </div>
+  );
 }
