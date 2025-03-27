5148c4e375f9a46829eb2c4d072bf452172fadef
doublN <n.newberry@hotmail.com>
2025-03-27 16:57:32 +0000

    add FinanceTracker to dashboard, adjust styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
+import { FinanceTracker } from "@/components/FinanceTracker";
+
 export default function Home() {
   return (
     <div className="h-full flex flex-col gap-4 my-10 mx-6">
-      <div className="border-2 border-gray-200 rounded-lg bg-white">
-        Tracker
-      </div>
+      <FinanceTracker />
       <div className="grid grid-cols-2 grid-rows-2 grow gap-4">
-        <div className="row-span-2 border-2 border-gray-200 rounded-lg bg-white">
+        <div className="row-span-2 border-2 border-gray-200 rounded-xl bg-white">
           My Portfolio
         </div>
-        <div className="border-2 border-gray-200 rounded-lg bg-white">
+        <div className="border-2 border-gray-200 rounded-xl bg-white">
           stats
         </div>
-        <div className="border-2 border-gray-200 rounded-lg bg-white">
+        <div className="border-2 border-gray-200 rounded-xl bg-white">
           Fear and Greed
         </div>
       </div>
-      <div className="grow border-2 border-gray-200 rounded-lg bg-white">
+      <div className="grow border-2 border-gray-200 rounded-xl bg-white">
         Crypto Market
       </div>
     </div>
