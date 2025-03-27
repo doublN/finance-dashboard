936844e6589de01755ceab70b57790b82e164361
doublN <n.newberry@hotmail.com>
2025-03-27 17:32:05 +0000

    correct styling to keep content on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { FinanceTracker } from "@/components/FinanceTracker";
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col gap-4 my-10 mx-6">
+    <div className="h-full flex flex-col gap-4 py-10 px-6">
       <FinanceTracker />
       <div className="grid grid-cols-2 grid-rows-2 grow gap-4">
         <div className="row-span-2 border-2 border-gray-200 rounded-xl bg-white">
