a114298d681ed321ccab27a6a9a1461d16cabb2f
doublN <n.newberry@hotmail.com>
2025-03-28 18:21:57 +0000

    create generic Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+type Props = {
+  readonly children: ReactNode | Array<ReactNode>;
+};
+
+export function Card(props: Props) {
+  return (
+    <div className="flex flex-col row-span-1 col-span-2 border-2 border-gray-200 rounded-xl bg-white py-6 px-5">
+      {props.children}
+    </div>
+  );
+}
