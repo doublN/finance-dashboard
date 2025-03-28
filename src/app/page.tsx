b15fbb7c17b36993a33c9252697070137107f93c
doublN <n.newberry@hotmail.com>
2025-03-28 18:06:46 +0000

    add PortfolioStatsCard to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import { FinanceTracker } from "@/components/FinanceTracker";
+import { PortfolioStatsCard } from "@/components/PortfolioStatsCard";
 
 export default function Home() {
   return (
     <div className="h-full flex flex-col gap-4 py-10 px-6">
       <FinanceTracker />
-      <div className="grid grid-cols-2 grid-rows-2 grow gap-4">
-        <div className="row-span-2 border-2 border-gray-200 rounded-xl bg-white">
+      <div className="grid grid-cols-5 grid-rows-2 grow gap-4 max-h-[50vh]">
+        <div className="col-span-3 row-span-2 border-2 border-gray-200 rounded-xl bg-white">
           My Portfolio
         </div>
-        <div className="border-2 border-gray-200 rounded-xl bg-white">
-          stats
-        </div>
-        <div className="border-2 border-gray-200 rounded-xl bg-white">
+        <PortfolioStatsCard />
+        <div className=" col-span-2 row-span-1 border-2 border-gray-200 rounded-xl bg-white">
           Fear and Greed
         </div>
       </div>
