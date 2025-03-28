e1532e1dfab72d4da621051cc745cb96257539ee
doublN <n.newberry@hotmail.com>
2025-03-28 18:22:27 +0000

    add Card component to PortfolioStatsCard component

--- src/components/PortfolioStatsCard.tsx
+++ src/components/PortfolioStatsCard.tsx
@@ -1,8 +1,9 @@
+import { Card } from "./Card";
 import { PortfolioStatsRanking } from "./PortfolioStatsRanking";
 
 export function PortfolioStatsCard() {
   return (
-    <div className="flex flex-col row-span-1 col-span-2 border-2 border-gray-200 rounded-xl bg-white py-6 px-4">
+    <Card>
       <div className="flex items-center justify-between">
         <h3 className="text-xl font-semibold">Portfolio Stats</h3>
         <button className="text-blue-900 font-semibold hover:cursor-pointer">
@@ -10,6 +11,6 @@ export function PortfolioStatsCard() {
         </button>
       </div>
       <PortfolioStatsRanking />
-    </div>
+    </Card>
   );
 }
