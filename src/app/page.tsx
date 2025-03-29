06fe3c11a1b88e946d5beec9e4c3d7934a6cdc95
doublN <n.newberry@hotmail.com>
2025-03-29 22:02:36 +0000

    set height of grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="h-full flex flex-col gap-4 py-10 px-6">
       <FinanceTracker />
-      <div className="grid grid-cols-5 grid-rows-2 grow gap-4 max-h-[50vh]">
+      <div className="grid grid-cols-5 grid-rows-2 grow gap-4 h-[100vh]">
         <div className="col-span-3 row-span-2 border-2 border-gray-200 rounded-xl bg-white">
           My Portfolio
         </div>
