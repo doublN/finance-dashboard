ac8ffc0a79a47f4c50a24cd829e5518542e45bcc
doublN <n.newberry@hotmail.com>
2025-03-29 16:14:03 +0000

    adjust style on table

--- src/components/CryptoTable.tsx
+++ src/components/CryptoTable.tsx
@@ -18,11 +18,12 @@ export type CryptoTableRow = {
 };
 
 type Props = {
-  data: Array<CryptoTableRow>;
+  readonly data: Array<CryptoTableRow>;
 };
 
 export function CryptoTable(props: Props) {
-  const headerStyle = "py-2 text-left";
+  const headerStyle =
+    "py-2 text-left text-sm text-gray-500 border-t-2 border-b-2 border-gray-200";
 
   const iconColor = {
     paypal: "bg-paypal",
@@ -33,10 +34,10 @@ export function CryptoTable(props: Props) {
   };
 
   return (
-    <table>
+    <table className="table-auto border-separate border-spacing-y-6 flex-1 overflow-y-scroll">
       <thead>
         <tr className="text-sm text-gray-500 border-t-2 border-b-2 border-gray-200">
-          <th> </th>
+          <th className={headerStyle}> </th>
           <th className={headerStyle}>NAME</th>
           <th className={headerStyle}>PRICE</th>
           <th className={headerStyle}>1H %</th>
@@ -45,10 +46,46 @@ export function CryptoTable(props: Props) {
           <th className={headerStyle}>VOLUME i</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="">
         {props.data.map((rowData) => (
-          <tr key={rowData.currency.name}>
-            <td className="py-10">*</td>
+          <tr key={rowData.currency.label}>
+            <td>*</td>
+            <td>
+              <div className="flex items-center gap-3">
+                <div
+                  className={`w-10 h-10 rounded-full ${
+                    iconColor[rowData.currency.name]
+                  }`}
+                ></div>
+                <div>
+                  <p className="font-semibold">{rowData.currency.label}</p>
+                  <p className="text-sm text-gray-500">
+                    {rowData.currency.abbreviation}
+                  </p>
+                </div>
+              </div>
+            </td>
+            <td>${rowData.price}</td>
+            <td>
+              <PercentageChangeLabel value={rowData.oneHour.toString()} />
+            </td>
+            <td>
+              <PercentageChangeLabel
+                value={rowData.twentyFourHour.toString()}
+              />
+            </td>
+            <td>${rowData.marketCap}</td>
+            <td>
+              <p>${rowData.volume.dollarValue}</p>
+              <p className="text-sm text-gray-500">
+                {rowData.volume.cryptoValue} {rowData.currency.abbreviation}
+              </p>
+            </td>
+          </tr>
+        ))}
+        {props.data.map((rowData) => (
+          <tr key={rowData.currency.label}>
+            <td>*</td>
             <td>
               <div className="flex items-center gap-3">
                 <div
