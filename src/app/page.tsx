6f068d2b573e939fd20c900b46533942c28e2981
doublN <n.newberry@hotmail.com>
2025-03-31 17:19:39 +0100

    add MyPortfolioCard to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { CryptoMarketCard } from "@/components/CryptoMarketCard";
 import { FearAndGreedCard } from "@/components/FearAndGreedCard";
 import { FinanceTracker } from "@/components/FinanceTracker";
+import { MyPortFolioCard } from "@/components/MyPortfolioCard";
 import { PortfolioStatsCard } from "@/components/PortfolioStatsCard";
 
 export default function Home() {
@@ -8,8 +9,8 @@ export default function Home() {
     <div className="h-full flex flex-col gap-4 py-10 px-6">
       <FinanceTracker />
       <div className="grid grid-cols-5 grid-rows-2 grow gap-4 h-[60vh]">
-        <div className="col-span-3 row-span-2 border-2 border-gray-200 rounded-xl bg-white">
-          My Portfolio
+        <div className="col-span-3 row-span-2 border-2 py-6 px-5 border-gray-100 rounded-xl bg-white">
+          <MyPortFolioCard />
         </div>
         <PortfolioStatsCard />
         <FearAndGreedCard />
