e9d5e4615eb5713228bb73517af4bf479903fb90
doublN <n.newberry@hotmail.com>
2025-03-31 17:19:11 +0100

    Create StatisticsTableRangeWidget placeholder

---
+++ src/components/StatisticsTableRangeWidget.tsx
@@ -0,0 +1,21 @@
+export function StatisticsTableRangeWidget() {
+  return (
+    <div>
+      <span>
+        <button>1D</button>
+      </span>
+      <span>
+        <button>7D</button>
+      </span>
+      <span>
+        <button>1M</button>
+      </span>
+      <span>
+        <button>3M</button>
+      </span>
+      <span>
+        <button>AL</button>L
+      </span>
+    </div>
+  );
+}
