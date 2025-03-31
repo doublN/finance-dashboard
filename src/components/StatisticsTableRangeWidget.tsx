9837acbcd32b0d4d64cafe6cd0969230720a4ac3
doublN <n.newberry@hotmail.com>
2025-03-31 21:41:52 +0100

    finish StatisticsTableRangeWidget

--- src/components/StatisticsTableRangeWidget.tsx
+++ src/components/StatisticsTableRangeWidget.tsx
@@ -1,21 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
 export function StatisticsTableRangeWidget() {
+  const [selected, setSelected] = useState("7D");
+
+  const values = ["1D", "7D", "1M", "3M", "ALL"];
+
   return (
-    <div>
-      <span>
-        <button>1D</button>
-      </span>
-      <span>
-        <button>7D</button>
-      </span>
-      <span>
-        <button>1M</button>
-      </span>
-      <span>
-        <button>3M</button>
-      </span>
-      <span>
-        <button>AL</button>L
-      </span>
+    <div className="p-2 bg-gray-100 rounded-xl">
+      {values.map((value) => (
+        <button
+          key={value}
+          className={`py-1 px-2 ${
+            value === selected ? "bg-white drop-shadow-sm" : "text-gray-400"
+          }  rounded-lg font-semibold hover:cursor-pointer`}
+          onClick={() => {
+            setSelected(value);
+          }}
+        >
+          <span>{value}</span>
+        </button>
+      ))}
     </div>
   );
 }
