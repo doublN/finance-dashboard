0f17649effe86ea1acb6687a088084ea12211266
doublN <n.newberry@hotmail.com>
2025-03-31 17:19:27 +0100

    create MyPortfolioCard

---
+++ src/components/MyPortfolioCard.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { numberFormat } from "@/utils/numberFormat";
+import { PercentageChangeLabel } from "./PercentageChangeLabel";
+import { StatisticsTable } from "./StatisticsTable";
+
+export function MyPortFolioCard() {
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <div>
+          <h3 className="text-xl font-semibold">My Portfolio</h3>
+          <div className="mt-3 flex items-center gap-3">
+            <h1 className="text-3xl">
+              <span className="text-gray-500">$</span>{" "}
+              <span className="font-semibold">
+                {numberFormat.format(14089.87)}
+              </span>{" "}
+            </h1>
+            <PercentageChangeLabel value="5.95" />
+          </div>
+        </div>
+        <div>DROPDOWN</div>
+      </div>
+      <hr className="text-gray-100 border-1 my-6" />
+      <StatisticsTable />
+    </div>
+  );
+}
